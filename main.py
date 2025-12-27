7b46d783428caac0bf8c4c762776f3189fdba18b
Farooqsultan047 <farooqsultan047@gmail.com>
2025-12-27 22:03:20 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,441 @@
+# name = "farooq sultan"
+# age = 25
+# height = 5.9
+
+# print(" my Name is :", name , "age is ", age, "Height: awan dai", height) 
+# a = "10"
+# b=20.3
+# sum = a - b
+# print(type(a))
+# a = float(10)
+# b = int(20.3)
+# print(type(a))
+# print(type(b))
+# a = "10.5"
+# b = float(a) + 20.3
+# print(type(b))
+# print(b)
+# a = "10"
+# a = str(a)
+# print(type(a))
+# name = input("Enter your name: ")
+# print("Your name is: ", name)
+# val = input("Enter a some value: ")
+# print(type(val), val)
+# int ("4")
+# val = int (input("Enter a number:"))
+# print(type(val), val)
+# name = input("Enter a name ")
+# age = int (input("enter a age"))
+# marks = float (input("enter a marks "))
+# print("wellcome:" , name)
+# print ("age =", age)
+# print ("marks:", marks)
+# first = int(input("Enter a first: "))
+# second = int(input("Enter a second: "))
+# print(" number a sum = ", first + second)
+# side = float(input(" Enter squre side:"))
+# print("area=", side ** 2)
+# a = float(input(" enter first :"))
+# b = float(input(" enter second :"))
+# print("avg = ", (a+b)/2)
+# a = int(input(" enter first :"))
+# b = int(input(" enter second :"))
+# print(a >=b)
+#                        string concatenation
+# string1 = "hello"
+# string2 = "world"
+# print(string1 + " " + string2)
+# string1 = (" enter first string \t  my name is farooq")
+# # string2 = (" enter second string")
+# print(string1)
+# str1 = "farooq"
+# len1 = len(str1)
+# str2 = "sultan"
+# len2 = len(str2)
+# print("length of str1:", len1)
+# print("length of str2:", len2) 
+# # print("total length:", len1 + len2)
+# str1 = input(" enter first string :")
+# str2 = input(" enter second string :")
+# len1 = len(str1)
+# len2 = len(str2)
+# print("length of str1:", len1)
+# print("length of str2:", len2)
+        #  slice
+# str1 = "farooq sultan"
+# chr = str1[4]
+# print(chr)
+# sub_str = str1[0:6]
+# print(sub_str)
+# str1 = "farooq sultan"
+# print(str1[3:9])
+# str1 = "farooq sultan"
+# print(str1[5:len(str1)])
+# str1 = "farooq sultan"
+# # print(str1[-6:-1])
+                                              # string functions
+# str1 = "farooq sultan"
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.capitalize())     
+# print(str1.title())
+# print(str1.count("a"))
+# print(str1.find("sultan")) 
+# print(str1.replace("farooq", "ahmed"))     
+# print(str1.split(" "))   
+# print(len(str1))     
+# print(str1.isalpha())    
+# marks = int(input("Enter your marks (or -1 to exit): "))
+# if marks == -1:
+#         break
+# if marks >= 90:
+#         grade = "A+"
+# elif marks >= 80:
+#         grade = "A"
+# elif marks >= 70:
+#         grade = "B"
+# elif marks >= 60:
+#         grade = "C"
+# elif marks >= 50:
+#         grade = "D"
+# else:
+#         grade = "F"
+# print("Your grade is:", grade)
+# age = 95
+
+# if age >= 18:
+#     if age >= 65:
+#         print("You are not eligible to work.")
+#     else:
+#         print("You are eligible to work.")
+# else:
+#     print("You are not eligible to work.")
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+# num = int(input("Enter a number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# if num >= num2 and num >= num3:
+#     largest = num
+# elif num2 >= num and num2 >= num3:
+#     largest = num2
+# else:
+#     largest = num3
+#     print("The largest number is:", largest)
+# x = int(input("Enter a number: "))
+# if (x % 7==0):
+#     print("multiple of 7")
+# else:
+#     print("not multiple of 7")
+#     list in python
+# marks1 = 94.2
+# marks2 = 94.2
+# marks3 = 80
+# marks3 = 70
+# marks4 = 68
+# marks5 = 45
+# print(marks5)
+# marks = (10,20,30,40,50)
+# print(marks)
+# print(type(marks))
+# student = ["farooq", 24.0,17,"sultan"]
+# print(student[2:3])
+# list = [1,2,3]
+# list.append(4)
+# print(list)
+# list = [3,1,2]
+# list.append(4)
+# list.sort()
+# print(list)
+# list = [3,1,2]
+# list.append(4)
+# print(list.sort(reverse=True))
+# print(list)
+# list = [1,2,3]
+# list.insert(1,5)
+# print(list)
+# list = (1,2,3,4)
+# list.pop(2)
+# print(list)
+# tuple = (1,2,3,4,5,)
+# # print(type(tuple))
+# print(tuple)
+# movies = []
+# move1 = input ("enter a 1st moves: ")
+# move2 = input ("Enter a 2nd moves: ")
+# move3 = input ("Enter a 3rd movie: ")
+# movies.append(move1)
+# movies.append(move2)
+# movies.append(move3)
+# print(movies)
+
+# list1 = [1, 2, 3, 2, 1,]
+# copy_list = list1.copy()
+# copy_list.reverse()
+
+# if copy_list == list1:
+#     print("Palindrome")
+# else:
+#     print("NOT Palindrome")
+# grade = ("A", "B", "C", "D", "D", "D")
+# print(grade.count("D"))
+# grade = ["E", "B", "F", "A", "C", "D"]
+# grade.sort()
+# print(grade)
+                        #  dictionary
+# info ={    
+# "name" : "Farooq Sultan",
+# "Age" : 26,
+# # "Learning" : "codeing",
+        
+# # }
+# # print(info)
+# # info ={    
+# # "name" : "Farooq Sultan",
+# # "Age" : 26,
+# # "Learning" : "codeing",
+# # 26: TimeoutError,
+# # "student" :{     
+# # "phy" : 24,
+# # "comp" : 30,
+# # "math" : 40,    
+# # }
+# # }
+# # print(info)
+# #  
+# # print(len(list(info.copy())))
+# # print(info.values())
+# # print(list(info.items()))
+# # print(info["student"]["phy"])
+# # dir = {
+# #         "Name" : "farooq",
+# #         "age" : 28,
+# #         "Marks " : 90,
+        
+# # }
+# # # print(dir["Name"])
+# # print(dir.get("Name2"))
+# #        sets 
+# # collection = {1, 2, 3, 4}
+# # print(callable)
+# # print(collection)
+# # print (type(collection))
+# collection = set()
+# # print(collection)
+# collection.add(1)
+# collection.add(9)
+# collection.add(1)
+# collection.add(1)
+# collection.add(7)
+# collection.add(1)
+# collection.add(6)
+# collection.add(1)
+# collection.add(1)
+# collection.add(5)
+# collection.add(1)
+# collection.add(1)
+
+# collection.add(3)
+# collection.add(1)
+# collection.add(2)
+ 
+# collection.remove(1)
+# print(collection)
+# set = {1,2,3,4,5,6,7,8,9,0}
+# tupal = (1,2,3,4,5,6,7,8,9,0,)
+# list= [1,2,3,4,5,6,7,8,9,0,]
+# collection = {"farooq", "Sultan", "Awan","Malik"}
+# print(collection.pop())
+# print(collection.pop())
+# print(collection.pop())
+# print(collection.pop())
+# set1 = {1,2,3,4,5,6}
+# set2 = {1,2,3,4,5,6,7,8,9,0}
+# print(set1.intersection(set2))
+# print(set2.union(set1))
+# from turtle import *
+# speed(0)
+# bgcolor("black")
+# colors = ['orange', 'white']
+# hideturtle()
+
+# for i in range(122):
+#     penup()
+#     goto(0, 0)
+#     pendown()
+#     color(colors[i % 2])
+#     forward(130)
+#     left(3)
+#     circle(40)
+#     forward(130)
+#     
+# while looop 
+# count = 1  
+# while count <= 5:
+#       print("Hello")
+#       count += 1
+#       print (count)
+        #        range
+# print(range(5))
+        #    linear searuch
+# print(type(range(5)))
+# for i in range(10):
+#         print(i)
+# for i in range(5,12,3):
+#         print(i)
+# n= int(input("Enter a number "))
+# for p in range(1, 11):
+#          print(p * n)
+# for p in range (5):
+#         pass
+# print("Works in power full")
+# n = 3
+# fact = 1
+# i = 2
+# while  i <= n:
+#     fact *= i 
+#     i += 3
+# print(" factorial is", fact)
+        
+# n = 3
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+
+# print("Factorial is:", fact)
+                                                           #   recursion 
+        # itself call
+ 
+
+# def show(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     show(n - 1)
+
+# show(5)
+        #    factorial
+# def fact(n):
+#      if n == 0 or n == 1:
+#          return 1
+#      else:
+#          return fact(n - 1) * n
+
+# print(fact(5))
+# def fact(n):
+#          if n == 0 or n == 1:
+#                  return 1
+#          else:
+#                  return fact (n - 1) * n
+# print(fact(6))
+# def calc_sum(n):
+#         if (n==0):
+#                 return 0
+#         return calc_sum(n-1) + n
+# sum = calc_sum(70)
+# print(sum)
+# def print_list(list, idx=0):
+#     if idx == len(list):
+#         return
+#     print(list[idx])
+#     print_list(list, idx + 1)
+
+# fruits = ["mango", "litchi", "apple", "banana"]
+
+# def print_list(list, index=0):
+#     if index == len(list):
+#         return
+#     print(list[index])
+#     print_list(list, index + 1)
+
+# fruits = ["mango", "frute", "litchi"]
+# print_list(fruits)
+   
+#                                         file
+#   towe type 
+#   text file 
+#   binary file
+
+# f=open("demo.text","r")
+# data = f.read() 
+# print(data)
+# f.close()
+# f = open("demo.text","r")
+# line1 = f.readline()
+# print(line1)
+
+# line2 = f.readline()
+# print(line2)
+# f.close()
+# f = open("demo.text", "a")
+# f.write("/n write the data and represent and no for word and help war no")
+# f.close
+# f = open("demo.text","+")
+# f.write("malik")
+# f.close()
+# with open("demo.txt","r") as f:
+#       data =f.read()
+# print(data)
+# with open("demo.txt","w") as f:
+#       f.write("Pakistan")
+# import os
+# os.remove("demo.txt")
+# with open("paratics.txt", "w") as f:
+#     f.write("He everyone\nWe learning file IO\n")
+#     f.write("Using Java.\nLike programming in Java" )
+# with open("practic.txt", "r") as f:
+#     data = f.read()
+# new_data = data.replace("Java", "Python")
+# print(new_data)
+# with open("practic.txt", "w") as f:
+#     f.write(new_data)
+# def check_for_word():
+#     word = " learning"
+#     with open("practic.txt", "r") as f:
+#         data = f.read()
+#         if word in data:
+#             print("Found")
+#         else:
+#             print("Not found")
+# def check_word_line():
+#     word = "learning"
+
+#     with open("practic.txt", "r") as f:
+#         lines = f.readlines()
+#         found = False
+
+#         for i, line in enumerate(lines, start=1):
+#             if word in line:
+#                 print(f"'{word}' line {i} mein mila hai.")
+#                 found = True
+
+#         if not found:
+#             print(f"'{word}' kisi bhi line mein nahi mila.")
+
+# # function call
+# check_word_line()
+
+
+def check_word_line():
+    word = "learning"
+
+    with open("practic.txt", "r") as f:
+        found = False
+        for i, line in enumerate(f, start=1):
+            if word in line:
+                print(f"'{word}' line {i} mein mila hai.")
+                found = True
+
+        if not found:
+            print(f"'{word}' kisi bhi line mein nahi mila.")
+
+check_word_line()
+
+
+#                                    opp    # 
